893d874ac580346f78bee3bf801b513bf81e3fb5
Benjamin Odell <benjamin.m.odell@proton.me>
2026-03-28 18:23:21 -0500

    Installed notes app into NotesApp and create a migration

--- notes/NotesApp/settings.py
+++ notes/NotesApp/settings.py
@@ -31,6 +31,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'notes.apps.NotesConfig',
     'polls.apps.PollsConfig',
     'django.contrib.admin',
     'django.contrib.auth',
