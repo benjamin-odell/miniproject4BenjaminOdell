15d3933bf332ec843e799bef54dcf089422bac5c
Benjamin Odell <benjamin.m.odell@proton.me>
2026-03-28 18:21:59 -0500

    Added notes model

--- notes/notes/models.py
+++ notes/notes/models.py
@@ -1,3 +1,10 @@
+from django.contrib.auth.models import User
 from django.db import models
 
 # Create your models here.
+class Note(models.Model):
+    title = models.CharField(max_length=100)
+    subject = models.CharField(max_length=100)
+    content = models.TextField()
+    pub_date = models.DateTimeField("date published")
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
