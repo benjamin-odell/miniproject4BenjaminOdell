813152ae80a16363798a497c81cd555a64745abd
Benjamin Odell <benjamin.m.odell@proton.me>
2026-03-29 01:50:22 -0500

    Adds user creation, integrates base template, and prepares auth views.

--- notes/notes/urls.py
+++ notes/notes/urls.py
@@ -4,4 +4,5 @@ from . import views
 
 urlpatterns = [
     path('', views.index, name='index'),
+    path('create_user', views.create_user, name='create_user'),
 ]
--- notes/notes/views.py
+++ notes/notes/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render
 from django.contrib.auth.decorators import login_required
+from django.contrib.auth import authenticate, login, logout
 
 # Create your views here.
 def index(request):
@@ -8,3 +9,16 @@ def index(request):
 @login_required
 def note(request):
     return
+
+def create_user(request):
+    if request.method == "POST":
+        #create user
+        pass
+    else:
+        #render page
+        return render(request, 'notes/create_user.html')
+
+def logout_view (request):
+    logout(request)
+
+
