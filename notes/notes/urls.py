6610cb3d3fa15cd2276788520dad9fac0b722d3a
Benjamin Odell <benjamin.m.odell@proton.me>
2026-03-29 02:16:12 -0500

    Implements login view and refines authentication links.

--- notes/notes/urls.py
+++ notes/notes/urls.py
@@ -5,4 +5,6 @@ from . import views
 urlpatterns = [
     path('', views.index, name='index'),
     path('create_user', views.create_user, name='create_user'),
+    path('logout', views.logout_view, name='logout'),
+    path('login', views.login_view, name='login'),
 ]
--- notes/notes/views.py
+++ notes/notes/views.py
@@ -1,6 +1,8 @@
+from django.http import HttpResponseRedirect
 from django.shortcuts import render
 from django.contrib.auth.decorators import login_required
 from django.contrib.auth import authenticate, login, logout
+from django.urls import reverse
 
 # Create your views here.
 def index(request):
@@ -20,5 +22,28 @@ def create_user(request):
 
 def logout_view (request):
     logout(request)
+    return HttpResponseRedirect(reverse("index"))
+
+def login_view (request):
+    error = None
+    if request.method == "POST":
+        username = request.POST.get("username")
+        password = request.POST.get("password")
+
+        if username is None:
+            error = "Username is required."
+        elif password is None:
+            error = "Password is required."
+
+        else:
+
+            user = authenticate(username=request.POST["username"], password=request.POST["password"])
+            if user is not None:
+                login(request, user)
+                return HttpResponseRedirect(reverse("index"))
+            else:
+                error = "Incorrect username or password."
+
+    return render(request, 'notes/login.html', {"error": error})
 
 
