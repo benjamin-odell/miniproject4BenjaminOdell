0d31056fa87712dcd7a3c0113393276b512162db
Benjamin Odell <benjamin.m.odell@proton.me>
2026-04-01 06:40:33 -0500

    Configures Note admin to display title, user, date, and public status.

--- notes/notes/admin.py
+++ notes/notes/admin.py
@@ -1,5 +1,12 @@
 from django.contrib import admin
+
 from .models import Note
 
 # Register your models here.
-admin.site.register(Note)
+
+
+class NoteAdmin(admin.ModelAdmin):
+    list_display = ['title', 'user', 'pub_date', 'is_public']
+
+
+admin.site.register(Note, NoteAdmin)
